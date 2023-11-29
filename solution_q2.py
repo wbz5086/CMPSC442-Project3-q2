6e7ca0bbd681c36e030c1d00170a69fe16838d5d
wbz5086 <wbz5086@psu.edu>
2023-11-29 01:57:52 -0500

    finished 2.3

--- solution_q2.py
+++ solution_q2.py
@@ -23,6 +23,9 @@ class transFunc():
     def getProb(self, endstate: int) -> float:
         return self.estate.get(endstate) / self.samplesize
 
+    def getEndState(self) -> []:
+        return self.estate.keys()
+
     def print(self) -> int:
         node = self
         count = 0
@@ -33,16 +36,23 @@ class transFunc():
                 count += 1
         return count
     
+def tokey(inital: int, action: int) -> int:
+    return (inital << 4) + action
 
+# prints a square grid of size 4 by 4, given a size 16 array
+def printGrid(grid: []):
+    out = ""
+    for x in range(16):
+        if not x % 4:
+            out += "\n"
+        out += "{:0.2f} ".format(grid[x])
+    print(out)
 
 # Q2.2
 def learn(Rarray: []) -> {}:
     observation, info = env.reset()
     startState = observation
     out = {}
-
-    def tokey(inital: int, action: int) -> int:
-        return (inital << 4) + action
 
     for x in range(1000):
         terminated = 0
@@ -66,6 +76,37 @@ def learn(Rarray: []) -> {}:
 
     return out
 
+discount_factor = 0.9
+# Q2.3
+def study(T: {}, R: []) -> []:
+    V = R
+    # iteration layer
+    for i in range(50):
+        V1 = [0.0 for x in range(16)]
+        # for each state
+        for s in range(16):
+            m = 0.0
+            # for each action
+            for a in range(4):
+                if func := T.get(tokey(s, a)):
+                    # summation
+                    sum = 0
+                    for x in func.getEndState():
+                        sum += func.getProb(x) * (R[s] + discount_factor * V[x])
+                    m = max(sum, m)
+            if m == 0.0:
+                m = R[s]
+            V1[s] = m
+        V = V1
+        # printGrid(V)
+    return V
+
+
+def solve():
+    pass
+
+def apply():
+    pass
 
 if __name__ == "__main__":
     Rarray = [0.0 for x in range(16)]
@@ -75,6 +116,8 @@ if __name__ == "__main__":
     #     Tarray[x].print()
     # for x in Rarray:
     #     print("{} ".format(x))
+
+    Varray = study(Tarray, Rarray)
 
     env.close()
     
