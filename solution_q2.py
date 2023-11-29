4c192c1243813195c2a44e882221ad1d7ce864a3
wbz5086 <wbz5086@psu.edu>
2023-11-29 00:31:50 -0500

    redid solution for learning T and R

--- solution_q2.py
+++ solution_q2.py
@@ -1,61 +1,80 @@
 import gymnasium as gym
 env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=True)
 
-class transNode():
+class transFunc():
 
-    def __init__(self, startstate: int, action: int, endstate: int, reward: float) -> None:
+    def __init__(self, startstate: int, action: int, endstate: int) -> None:
         self.sstate = startstate
         self.estate = endstate
         self.action = action
-        self.reward = reward
-        self.next = None
+        self.estate = {}
+        self.estate[endstate] = 1
+        self.samplesize = 1
+        # self.next = None
 
-    def setNext(self, next) -> None:
-        self.next = next
+    # adds a single observation of ending on a state
+    def add(self, endstate: int) -> None:
+        if self.estate.get(endstate):
+            self.estate[endstate] += 1
+        else:
+            self.estate[endstate] = 1
+        self.samplesize += 1
+
+    def getProb(self, endstate: int) -> float:
+        return self.estate.get(endstate) / self.samplesize
 
     def print(self) -> int:
         node = self
         count = 0
-        while node is not None:
-            print("start: {},  end: {},  reward: {}\n".format(node.sstate, node.estate, node.reward))
-            if node.reward == 1:
+        for endstate in self.estate.keys():
+            print("start: {},  action: {},  end: {},  n: {},  chance: {}\n".format(node.sstate, node.action,
+                                                                            endstate, node.samplesize, node.getProb(endstate)))
+            if endstate == 15:
                 count += 1
-            node = node.next
         return count
+    
 
 
 # Q2.2
-def learn() -> []:
+def learn(Rarray: []) -> {}:
     observation, info = env.reset()
-    startState = 0
-    out = [None for p in range(1000)]
+    startState = observation
+    out = {}
+
+    def tokey(inital: int, action: int) -> int:
+        return (inital << 4) + action
+
     for x in range(1000):
         terminated = 0
         truncated = 0
-        node = transNode(-1, -1, startState, 0.0)
-        out[x] = node
         while not terminated and not truncated:
             action = env.action_space.sample() # agent policy that uses the observation and info
             observation, reward, terminated, truncated, info = env.step(action)
 
             # record to learning table
-            node.next = transNode(startState, action, observation, reward)
-            node = node.next
+            key = tokey(startState, action)
+            if out.get(key):
+                out[key].add(observation)
+            else:
+                out[key] = transFunc(startState, action, observation)
+                Rarray[observation] = reward
+            
             startState = observation
 
         observation, info = env.reset()
-        node.next = transNode(startState, -1, -1, 0.0)
-        startState = 0
-        # print("Observation: {}   Reward: {}   info: {}\n".format(observation, reward, info))
+        startState = observation
+
     return out
 
 
 if __name__ == "__main__":
-    learnarray = learn()
-    total = 0
-    for x in learnarray:
-        total += x.print()
-    print(total)
+    Rarray = [0.0 for x in range(16)]
+    Tarray = learn(Rarray)
+
+    # for x in sorted(Tarray.keys()):
+    #     Tarray[x].print()
+    # for x in Rarray:
+    #     print("{} ".format(x))
 
     env.close()
     
